87f5800910add8deaedc65da6c39fb7e986d3ba8
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2024-01-29 07:09:40 +0000

    Add portfolio

---
+++ app/routes/portfolio._index.tsx
@@ -0,0 +1,4 @@
+// Outlet is here.
+export default function PortfolioIndexRoute() {
+    return <div className="contents-route">Please select a contents</div>;
+}
---
+++ app/types.ts
@@ -0,0 +1,25 @@
+export type Experience = {
+    id: string;
+    company: string;
+    position: string;
+}
+
+export type IArticles = {
+    title: string;
+    url: string;
+    id: string;
+};
+
+export type Certificates = {
+    blockchainId: string;
+    title: string;
+}
+
+export type CombinedJson = {
+    myname: string;
+    githubProfile: string;
+    twitterProfile: string;
+    displayExperience: Experience[];
+    displayArticles: IArticles[];
+    displayCertificates: Certificates[];
+}
